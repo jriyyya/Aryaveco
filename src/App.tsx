67801660f1acae057244f8e78e57b35502cd4105
Riya Jain <jriya.shi@gmail.com>
2023-06-23 12:48:27 +0530

    bg image fix

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ export default function App() {
 function Root() {
   return (
     <main className="relative">
-      <div className="bg-[url('/images/hero.png')] absolute top-0 left-0 w-full h-full -z-10" />
       <Navbar />
       <Outlet />
       <Footer />
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,11 @@ import WhyUs from "./components/WhyUs";
 export default function HomePage() {
   return (
     <div className="p-page">
+      <div className="absolute -z-10 top-0 w-full h-full left-0">
+        <img src="/images/hero.png" alt="" />
+        <div className="absolute top-[50vh] left-0 w-full h-[150vh] bg-gradient-to-b from-transparent via-primary to-transparent opacity-30" />
+        <img src="/images/water-bg.webp" alt="" />
+      </div>
       <Hero />
       <WhyUs />
     </div>
