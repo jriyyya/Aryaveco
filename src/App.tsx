70e990d94b7a7f5d8ba775f32382ef8d59bdcb6a
Riya Jain <jriya.shi@gmail.com>
2023-06-24 19:55:31 +0530

    Hero about section

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ import Navbar from "./common/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
 import { GlobalContextProvider } from "./contexts/globalContext";
 import { CacheContextProvider } from "./contexts/cacheContext";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<HomePage />} />
-        <Route path="/0" element={<div />} />
+        <Route path="/about" element={<AboutPage />} />
       </Route>
     )
   );
---
+++ src/pages/AboutPage/components/Hero.tsx
@@ -0,0 +1,30 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-row gap-x-16 mr-24 py-20">
+      <div className="self-center text-primary font-semibold text-right">
+        <div>Who are we?</div>
+        <div>What do we do?</div>
+        <div>Why do we do this?</div>
+      </div>
+      <div className="basis-2/3 flex flex-col gap-y-6">
+        <div className="text-primary text-5xl font-bold">About Us</div>
+        <div className="font-semibold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nulla
+          laboriosam minus ab odit exercitationem dolorum tempora consequuntur
+          iusto ipsa eum, sequi error velit porro nesciunt libero possimus
+          placeat voluptatibus quas cum! Ullam animi, reiciendis temporibus
+          maiores voluptatibus provident harum dolorum sunt voluptatem voluptate
+          <br />
+          <br />
+          sint rerum, tempore officiis iste nemo eum nam illo facere at eaque
+          debitis. Facere, quia illum maiores veritatis minus eum consequatur
+          alias labore veniam sed illo quam aperiam obcaecati porro dolor
+          dolorem molestiae voluptas officia, inventore sapiente temporibus,
+          pariatur vitae. Eveniet pariatur quia repellendus iusto hic laudantium
+          commodi distinctio voluptates veritatis, adipisci, expedita obcaecati
+          labore nesciunt.
+        </div>
+      </div>
+    </div>
+  );
+}
