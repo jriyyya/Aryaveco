7a77aff67dd27f472b716bf7fcf7cf7b36bb78a8
Riya Jain <jriya.shi@gmail.com>
2023-06-27 13:40:35 +0530

    Ran format

--- src/interfaces/Data.d.ts
+++ src/interfaces/Data.d.ts
@@ -1,16 +1,16 @@
 export interface Product {
-    name: string,
-    description: string,
-    imgUrl: string,
-    features: {
-        capacity: number,
-        storage: number,
-        power: number,
-        noise: number,
-        extras: string[]
-    },
-    highlights: {
-        title: string;
-        items: { key: string; value: string }[];
-    }[]
+  name: string;
+  description: string;
+  imgUrl: string;
+  features: {
+    capacity: number;
+    storage: number;
+    power: number;
+    noise: number;
+    extras: string[];
+  };
+  highlights: {
+    title: string;
+    items: { key: string; value: string }[];
+  }[];
 }
