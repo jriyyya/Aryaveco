7e52c8d20a357e6e1be1695112ca5e4bdca4a0fe
Riya Jain <jriya.shi@gmail.com>
2023-06-29 16:18:37 +0530

    Made task repsonisve

--- src/pages/AboutPage/components/Task.tsx
+++ src/pages/AboutPage/components/Task.tsx
@@ -24,27 +24,29 @@ const problems = [
 
 export default function Task() {
   return (
-    <section className="bg-background w-full px-24 py-16 mt-[30vh] border-primary border rounded-3xl flex flex-col justify-center gap-y-8 bg-opacity-60 backdrop-blur-md">
-      <div className="bg-black text-white w-max px-2 py-1 rounded-xl self-center text-xl">
+    <section className="bg-background w-full px-24 py-16 mt-[30vh] border-primary border rounded-3xl flex flex-col justify-center gap-y-8 mobile:gap-y-2 bg-opacity-60 backdrop-blur-md mobile:px-4 mobile:py-8">
+      <div className="bg-black text-white w-max px-2 py-1 rounded-xl self-center text-xl mobile:text-lg">
         Why do we do this?
       </div>
-      <div className="flex flex-col justify-center gap-y-16">
-        <div className="flex flex-col">
-          <div className="text-3xl font-semibold text-front self-center">
+      <div className="flex flex-col justify-center gap-y-16 mobile:gap-y-8">
+        <div className="flex flex-col  gap-y-4 mobile:gap-y-2">
+          <div className="text-3xl font-semibold text-front self-center text-center mobile:text-2xl">
             Problems we aim to solve
           </div>
-          <div className="flex flex-row self-center gap-x-4 pt-4">
+          <div className="flex flex-row self-center gap-x-4">
             <div className="w-[4vw] self-center bg-black h-1" />
             <div className="w-[4vw] self-center bg-black h-1" />
           </div>
         </div>
-        <div className="flex flex-row justify-around text-center">
+        <div className="flex flex-row justify-around text-center mobile:flex-wrap mobile:gap-y-4">
           {problems.map((problem, i) => (
-            <div key={i} className="w-[18%] flex flex-col gap-y-2">
+            <div key={i} className="w-[18%] flex flex-col gap-y-2 mobile:w-1/2">
               <div className="text-primary text-3xl font-bold">
                 {problem.title}
               </div>
-              <div className="font-semibold">{problem.description}</div>
+              <div className="font-semibold mobile:text-sm">
+                {problem.description}
+              </div>
             </div>
           ))}
         </div>
