a757a0c5c1e8e16a4cd422fd70c1bad675b65865
Riya Jain <jriya.shi@gmail.com>
2023-06-29 15:39:59 +0530

    made nav responsive

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="p-page relative overflow-hidden">
       <div className="absolute -z-10 top-0 w-full h-full left-0">
-        <img src="/images/hero.png" alt="" />
+        <img src="/images/hero.png" alt="" className="" />
         <div className="absolute top-[50vh] left-0 w-full h-[150vh] bg-gradient-to-b from-transparent via-primary to-transparent opacity-30" />
         <img src="/images/water-bg.webp" alt="" className="h-max" />
       </div>
